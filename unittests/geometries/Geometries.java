17fcc156d7b3c17a6c5d360d1722ed86c086cb3f
Tammy <rabinovitztami@gmail.com>
2025-04-08 21:12:50 +0300

    geometries composite

---
+++ unittests/geometries/Geometries.java
@@ -0,0 +1,28 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Geometries implements Intersectable{
+
+    final List<Intersectable> intersections = new LinkedList<Intersectable>();
+
+    public Geometries() {
+    }
+
+    public Geometries(Intersectable... geometries) {
+        add(geometries);
+    }
+
+    public void add(Intersectable... geometries){
+
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        return null;
+    }
+}
