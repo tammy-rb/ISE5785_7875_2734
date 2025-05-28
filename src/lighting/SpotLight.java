8011e970f82c9d452a3139fa27f8c559de5bc0b1
Tammy <rabinovitztami@gmail.com>
2025-05-28 10:39:22 +0300

    bonus stage6 narrow beam

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -5,14 +5,21 @@ import primitives.Point;
 import primitives.Vector;
 
 import static java.lang.Math.max;
+import static java.lang.Math.pow;
 import static primitives.Util.alignZero;
 
 public class SpotLight extends PointLight{
     private final Vector direction;
+    private double narrowBeam = 1; // default: no narrowing
 
     public SpotLight(Color intensity, Point position, Vector direction) {
         super(intensity, position);
         this.direction = direction.normalize();
+    }
+
+    public SpotLight setNarrowBeam(double narrowBeam) {
+        this.narrowBeam = narrowBeam;
+        return this;
     }
 
     @Override
@@ -32,7 +39,9 @@ public class SpotLight extends PointLight{
 
     @Override
     public Color getIntensity(Point p) {
-        return super.getIntensity(p).scale(max(0, alignZero(direction.dotProduct(getL(p)))));
+        return super.getIntensity(p).scale(
+                max(0, pow(alignZero(direction.dotProduct(getL(p))), narrowBeam))
+        );
     }
 
 }
